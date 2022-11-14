17ebeecfdde8206873df80afe4d27584c70f34c8
taguivalle <taguivalle140965@gmail.com>
2022-11-14 15:34:38 -0500

    change in the methods

--- intro_To_Objects_MethodsWith_Parameters.rb
+++ intro_To_Objects_MethodsWith_Parameters.rb
@@ -3,4 +3,9 @@ p 3.between?(1, 5)
 p 6.between?(1, 5)
 p 'cat'.between?('ant', 'dog')
 p 'gnu'.between?('ant', 'dog')
-p 
+p 20.between?(10, 30)
+p 20.between?(30, 50)
+p 1.2.between?(1.1, 1.3)
+p -10.5.between?(-20.1, 0)
+p 5.3.between?(8.8, 9.6)
+p
