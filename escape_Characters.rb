0982194b8516821a83699bdb8af72bb861e42b24
taguivalle <taguivalle140965@gmail.com>
2022-11-16 09:22:38 -0500

    Other lessons

---
+++ escape_Characters.rb
@@ -0,0 +1,23 @@
+puts "Hello world"
+puts 'Hello world'
+puts
+puts "Julieth be happy for happy birthday"
+puts "Julieth said 'Goodbye' to Guillermo"
+puts
+puts 'Julieth siad "Goodbye" to Guillermo'
+puts
+puts "Julieth  said \"Goodbye\" to Guillermo"
+puts
+puts 'Julieth said \'Goodbye\' to Guillermo'
+puts
+puts "Let's add a line break\nright here"
+puts
+puts "Let's add a line break\n right here with one space"
+puts
+result = "Let's add a line break\n right here with one space"
+p result
+puts
+puts "\tInsert a tab right here"
+puts
+puts "This normal swords, now with the tab\tother form the write"
+
