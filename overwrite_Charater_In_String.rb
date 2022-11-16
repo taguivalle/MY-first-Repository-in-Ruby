0b7a3e06422a96b8d4949abb4681db8ade3b2bc0
taguivalle <taguivalle140965@gmail.com>
2022-11-16 16:31:00 -0500

    Other lesson in the afternoon1

---
+++ overwrite_Charater_In_String.rb
@@ -0,0 +1,20 @@
+thing = "Rocket ship"
+p thing
+puts
+p thing[0] = "p"
+p thing
+puts
+thing[1] = "a"
+p thing
+puts
+thing[9] = "o"
+p thing
+puts
+fact = "I love bluberry pie"
+p fact[7, 4] = "rasp"
+p fact
+p fact[7..10] = "blue"
+p fact
+puts
+p fact[2..5] = "Absolutely adore"
+p fact
