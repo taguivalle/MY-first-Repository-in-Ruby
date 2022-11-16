d972b0ecf9758d79677116027f3bc0b9e5f00dfe
taguivalle <taguivalle140965@gmail.com>
2022-11-16 16:05:33 -0500

    change the name

---
+++ the_Length_And_Size_Methods.rb
@@ -0,0 +1,26 @@
+a = "Hello world"
+b = "Hi, my name is Tavo"
+c = " "
+d = ""
+puts
+p a.length
+p b.length
+p c.length
+p d.length
+puts
+p a.size
+p b.size
+p c.size
+p d.size
+puts
+p a.size.class
+p b.size.class
+p c.size.class
+p d.size.class
+puts
+p a.length.odd?.class
+p b.length.odd?.class
+p c.length.odd?.class
+p d.length.odd?.class
+
+
