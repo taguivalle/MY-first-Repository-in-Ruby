4dfeadcebb0c8534daa36d2ca2693c33494c334c
taguivalle <taguivalle140965@gmail.com>
2022-11-17 15:43:54 -0500

    That lesson 63

---
+++ return_Values.rb
@@ -0,0 +1,30 @@
+def add_two_numbers(num1, num2)
+    puts "OK, I am solving your math problem!"
+    return "Just kidding, I am a troll!"
+    return num1 + num2
+end
+p add_two_numbers(3,5)
+p add_two_numbers(3,8)
+p add_two_numbers(3,9)
+p add_two_numbers(3,10)
+puts
+def add_Other_numbers(num1, num2)
+    puts "OK, I am solving your other math problem!"
+    return "Just kidding, I am other troll!"
+    return num1 + num2
+end
+p add_Other_numbers(5,6)
+p add_Other_numbers(6,7)
+p add_Other_numbers(7,8)
+p add_Other_numbers(8,9)
+puts
+def add_moar_numbers(num1, num2)
+    puts "OK, I am solving moar math problem!"
+    return "Just kidding, I am moar troll!"
+    num1 + num2
+end
+p add_moar_numbers(120,15)
+p add_moar_numbers(101,15)
+p add_moar_numbers(106,15)
+p add_moar_numbers(105,15)
+
