9ea05fc58d9a23b24a6e49df3c308a54385d9014
taguivalle <taguivalle140965@gmail.com>
2022-11-17 14:28:36 -0500

    That is lesson 60

---
+++ intro_The_Methods.rb
@@ -0,0 +1,41 @@
+# A method is a grouped collection of Ruby statements that serves a purpose.
+# Methods are designed to be reused. Follow the golden rule: DRY.
+# Methods abstract the complexity of solving a problem.
+
+# Example: Convert Military Time (1630) to Standard Time (04:30 PM)
+# isolate first two numbers
+# If first numbers are less than or equal to 12, use them as the hour component and identify period as AM
+# Isolate last two numbers. Use them to determine the minutes component of final result.
+# Concatenate hour component, a colon, the minutes component, and the period.
+
+# Method Definitions and Calls
+# A method is first defined with a set Ruby statements.
+# The method es then called or invoked. These are programming terms for "run" or "execute".
+
+def introduce_myself
+    puts "I am handsome"
+    puts "I am brilliant"
+    puts "I am talented"
+end
+introduce_myself
+puts
+introduce_myself
+introduce_myself
+introduce_myself
+introduce_myself
+introduce_myself
+introduce_myself
+puts
+puts "I am handsome"
+puts "I am brilliant"
+puts "I am talented"
+puts "I am handsome"
+puts "I am brilliant"
+puts "I am talented"
+puts "I am handsome"
+puts "I am brilliant"
+puts "I am talented"
+puts "I am handsome"
+puts "I am brilliant"
+puts "I am talented"
+puts
