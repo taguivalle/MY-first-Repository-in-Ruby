437edb7371d0ca38a048904aacc75eac560ec49c
taguivalle <taguivalle140965@gmail.com>
2022-11-17 15:28:37 -0500

    That lesson es the number 62

---
+++ parameters_And_Arguments.rb
@@ -0,0 +1,14 @@
+def praise_person(name, age)
+    puts "#{name} is amazing"
+    puts "#{name} is charming"
+    puts "#{name} is talented"
+    puts "His age in one years will be is #{age + 1}"
+end
+praise_person"Boris", 25
+praise_person"Dave", 26
+praise_person"Pizza", 27
+puts
+praise_person("Boris", 25)
+praise_person("Dave", 26)
+praise_person("Pizza", 27)
+puts
