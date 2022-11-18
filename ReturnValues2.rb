f80f36cf5074eb412262b3b2400c9ccdba25f885
taguivalle <taguivalle140965@gmail.com>
2022-11-18 09:11:49 -0500

    That class 64

---
+++ ReturnValues2.rb
@@ -0,0 +1,15 @@
+def return_String
+    "What will be the return value here?"
+end
+puts
+def return_Guess
+    puts "Blah Blah Blah"
+    print "Will print be any diferente?"
+    puts false
+end
+p return_String
+p return_Guess
+puts
+result = return_Guess
+p result
+p result.class
