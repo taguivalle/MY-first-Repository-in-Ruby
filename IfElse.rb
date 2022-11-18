dbcdedc3a313d4c93cc565228b61f0bfdbff9652
taguivalle <taguivalle140965@gmail.com>
2022-11-18 16:10:15 -0500

    That's lesson number 68

---
+++ IfElse.rb
@@ -0,0 +1,78 @@
+grade = "C"
+if grade == "A"
+    puts "Taht's an excelent grade. Good Job!"
+elsif grade == "B"
+    puts "That'a a good grade. Let's bring it up nest time!"
+else 
+    puts "Unaceptable!"
+end
+puts
+grade = "A"
+if grade == "A"
+    puts "Taht's an excelent grade. Good Job!"
+elsif grade == "B"
+    puts "That'a a good grade. Let's bring it up nest time!"
+else 
+    puts "Unaceptable!"
+end
+puts
+grade = "B"
+if grade == "A"
+    puts "Taht's an excelent grade. Good Job!"
+elsif grade == "B"
+    puts "That'a a good grade. Let's bring it up nest time!"
+else 
+    puts "Unaceptable!"
+end
+grade = "D"
+if grade == "A"
+    puts "Taht's an excelent grade. Good Job!"
+elsif grade == "B"
+    puts "That'a a good grade. Let's bring it up nest time!"
+else 
+    puts "Unaceptable!"
+end
+grade = "BRandom gibberish"
+if grade == "A"
+    puts "Taht's an excelent grade. Good Job!"
+elsif grade == "B"
+    puts "That'a a good grade. Let's bring it up nest time!"
+else 
+    puts "Unaceptable!"
+end
+def odd_Or_Even(number)
+    if number.odd?
+        "That number is odd"
+    else
+        "That number is even"
+    end
+end
+p odd_Or_Even(5)
+puts
+def odd_Or_Even(number)
+    if number.odd?
+        "That number is odd"
+    else
+        "That number is even"
+    end
+end
+p odd_Or_Even(10)
+puts
+def odd_Or_Even(number)
+    if number.odd?
+        "That number is odd"
+    else
+        "That number is even"
+    end
+end
+p odd_Or_Even(98)
+puts
+def odd_Or_Even(number)
+    if number.odd?
+        "That number is odd"
+    else
+        "That number is even"
+    end
+end
+p odd_Or_Even(11)
+puts
