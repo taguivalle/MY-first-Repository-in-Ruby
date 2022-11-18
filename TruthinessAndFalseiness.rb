f36aca5036429c9a13ff541d2c3305060771c642
taguivalle <taguivalle140965@gmail.com>
2022-11-18 15:11:29 -0500

    That lesson is number 66

---
+++ TruthinessAndFalseiness.rb
@@ -0,0 +1,30 @@
+if 5 < 8
+    puts "That is true"
+end
+if 5
+    puts "That is true"
+end
+if false
+    puts "That is true"
+end
+if true
+    puts "That is true"
+end
+if -5
+    puts "That is true"
+end
+if -5 < 8
+    puts "That is true"
+end
+if []
+    puts "That is true"
+end
+if {}
+    puts "That is true"
+end
+if 5..19
+    puts "That is true"
+end
+if nil
+    puts "That is true"
+end
