f223ea2e9875887c9f9130bc6c21ed8e7e87fec2
taguivalle <taguivalle140965@gmail.com>
2022-11-18 15:52:08 -0500

    That lesson 67

---
+++ IfElsIf.rb
@@ -0,0 +1,81 @@
+color = "Green"
+if color == "Red"
+    puts "Red is red"
+elsif color == "Green"
+    puts "Green is great!"
+elsif color == "Yellow"
+    puts "Yes, Yellow the same is great!"
+elsif color == "Black"
+    puts "Yes, Black is other color great!"
+end
+puts
+color = "Red"
+if color == "Red"
+    puts "Red is red"
+elsif color == "Green"
+    puts "Green is great!"
+elsif color == "Yellow"
+    puts "Yes, Yellow the same is great!"
+elsif color == "Black"
+    puts "Yes, Black is other color great!"
+end
+puts
+color = "Yellow"
+if color == "Red"
+    puts "Red is red"
+elsif color == "Green"
+    puts "Green is great!"
+elsif color == "Yellow"
+    puts "Yes, Yellow the same is great!"
+elsif color == "Black"
+    puts "Yes, Black is other color great!"
+end
+puts
+color = "Black"
+if color == "Red"
+    puts "Red is red"
+elsif color == "Green"
+    puts "Green is great!"
+elsif color == "Yellow"
+    puts "Yes, Yellow the same is great!"
+elsif color == "Black"
+    puts "Yes, Black is other color great!"
+end
+puts
+number = 55
+if number < 25
+    puts "That is a low number"
+elsif number < 50
+    puts "Thats number in the middle!"
+elsif number < 75
+    puts "That's a big number"
+end
+puts
+number = 15
+if number < 25
+    puts "That's a low number"
+elsif number < 50
+    puts "Thats number in the middle!"
+elsif number < 75
+    puts "That's a big number"
+end
+puts
+number = 35
+if number < 25
+    puts "That's a low number"
+elsif number < 50
+    puts "Thats number in the middle!"
+elsif number < 75
+    puts "That's a big number"
+end
+puts
+umber = 55
+if number < 25
+    puts "That's a low number"
+elsif 70 < 100
+    puts "Thats number in the middle!"
+elsif number < 75
+    puts "That's a big number"
+end
+puts
+
