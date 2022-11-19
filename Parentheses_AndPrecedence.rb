f54965064bd24a19c22c28a0011e29cd65cd789f
taguivalle <taguivalle140965@gmail.com>
2022-11-19 10:05:38 -0500

    That's class number 71

---
+++ Parentheses_AndPrecedence.rb
@@ -0,0 +1,36 @@
+def authenticate_agent(rank, name, credentials)
+    if (rank == "007" && name == "James Bond") || credentials == "Secret Agent"
+        puts "Acces granted, please proceed to Intelligent department!"
+    else
+        puts "Acces denied, #{name}"
+    end
+end
+authenticate_agent("007", "James Bond", "Spy") # Note that there is another credential here
+puts
+def authenticate_agent(rank, name, credentials)
+    if (rank == "007" && name == "James Bond") || credentials == "Secret Agent"
+        puts "Acces granted, please proceed to Intelligent department!"
+    else
+        puts "Acces denied, #{name}"
+    end
+end
+authenticate_agent("007", "Jim Bond", "Spy") # Note that there is another credential here
+puts
+def authenticate_agent(rank, name, credentials)
+    if (rank == "007" && name == "James Bond") || credentials == "Secret Agent"
+        puts "Acces granted, please proceed to Intelligent department!"
+    else
+        puts "Acces denied, #{name}"
+    end
+end
+authenticate_agent("007", "Jim Bond", "Secret Agen") # Note that there is another credential here
+puts
+def authenticate_agent(rank, name, credentials)
+    if (rank == "007" && name == "James Bond") || credentials == "Secret Agent"
+        puts "Acces granted, please proceed to Intelligent department!"
+    else
+        puts "Acces denied, #{name}"
+    end
+end
+authenticate_agent("006", "Alek Trevelyan", "Secret Agent") # note that here there is another name and another rank
+puts
