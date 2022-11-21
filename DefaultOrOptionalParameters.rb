130b16b3132556824022b57080043a159006dc9b
taguivalle <taguivalle140965@gmail.com>
2022-11-21 15:01:25 -0500

    Thats leson number 75

---
+++ DefaultOrOptionalParameters.rb
@@ -0,0 +1,10 @@
+def make_phpne_call(number, international_code = 1, area_code = 64)
+    puts "Callign #{number}-#{international_code}-#{area_code}"
+end
+make_phpne_call(12345, 5, 9999)
+puts
+def makephpnecall(number, internationalCode = 1, areaCode = 64)
+    puts "Callign #{number}-#{internationalCode}-#{areaCode}"
+end
+makephpnecall("5555-5555", 5)
+puts
