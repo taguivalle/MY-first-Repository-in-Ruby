abb182ca7adead81b5e89a447d2bcd208173be9a
taguivalle <taguivalle140965@gmail.com>
2022-11-22 16:36:06 -0500

    That's class number 78

---
+++ Section Six Methods and Conditionals II/NegationWith!.rb
@@ -0,0 +1,13 @@
+user = "free"
+if user != "suscriber"
+    puts "Only suscribers are allowed!"
+end
+puts
+puts !true
+puts false
+puts !1
+puts !1.25
+puts !!1.25
+puts !nil
+puts !!nil
+puts !"Sapceship"
