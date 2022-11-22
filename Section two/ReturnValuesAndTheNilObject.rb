280aeddd00e3e729a0573249434d497d795008b5
taguivalle <taguivalle140965@gmail.com>
2022-11-22 15:41:05 -0500

    That's class number 27

---
+++ Section two/ReturnValuesAndTheNilObject.rb
@@ -0,0 +1,14 @@
+p 4
+p "Hello"
+puts
+p 5 +2 
+p "Hello " + "World"
+puts
+p "Harry potter".length
+puts
+p "harry " + "potter".upcase
+p "harry ".upcase + "potter"
+puts 5 + 2
+puts
+puts 5
+puts "Hello"
