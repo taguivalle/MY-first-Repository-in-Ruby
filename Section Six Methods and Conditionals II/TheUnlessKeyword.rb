c864dce5b469cfc26b5f39133650fe787cfe3840
taguivalle <taguivalle140965@gmail.com>
2022-11-22 17:43:39 -0500

    Thats class number 79

---
+++ Section Six Methods and Conditionals II/TheUnlessKeyword.rb
@@ -0,0 +1,52 @@
+password = "bestpasswordever"
+if  password != "topsecret"
+    puts "Not allowed"
+else
+    puts "That's the right password, welcome"
+end
+puts
+pass = "topsecret"
+if  pass != "topsecret"
+    puts "Not allowed"
+else
+    puts "That's the right password, welcome"
+end
+puts
+passw = "bestpasswordever"
+if  passw != "topsecret"
+    puts "That's the right password, welcome"
+else
+    puts "Not allowed"
+end
+puts
+passw = "topsecret"
+if  passw != "topsecret"
+    puts "That's the right password, welcome"
+else
+    puts "Not allowed"
+end
+puts
+passwo = "dominoes"
+unless  passwo == "topsecret"
+    puts "Not allowed"
+else
+    puts "That's the right password, welcome"
+end
+puts
+passwo = "dominoes"
+unless  passwo == "dominoes"
+    puts "Not allowed"
+else
+    puts "That's the right password, welcome"
+end
+puts
+password1 = "dominoes"
+if !password1.include?("a")
+    puts "It does not include the letter"
+end
+puts
+password2 = "password2"
+unless password2.include?("a")
+    puts "It does not include the letter"
+end
+puts
