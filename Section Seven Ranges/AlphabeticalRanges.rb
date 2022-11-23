b1b1857b85800c6a35dad41e0080fa9f9047068c
taguivalle <taguivalle140965@gmail.com>
2022-11-23 18:00:31 -0500

    That's class number 87

---
+++ Section Seven Ranges/AlphabeticalRanges.rb
@@ -0,0 +1,19 @@
+puts (1..10).first(3)
+puts
+print "The first alphabet\n"
+alphabet = "a".."z"
+puts alphabet.first(5)
+puts alphabet.last(1)
+puts
+print "The second alphabet\n"
+alphabet1 = "A".."z"
+puts alphabet1.first(5)
+puts alphabet1.last(1)
+puts
+print "The thirt alphabet\n"
+alphabet2 = "A".."Z"
+p alphabet2.first(40)
+puts alphabet2.first(5)
+puts alphabet2.last(40)
+puts
+
