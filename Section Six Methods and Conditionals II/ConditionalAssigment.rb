82e42a47f0ac564e379a3d89faf2cac4035dc99f
taguivalle <taguivalle140965@gmail.com>
2022-11-22 19:40:39 -0500

    That's class number 84

---
+++ Section Six Methods and Conditionals II/ConditionalAssigment.rb
@@ -0,0 +1,35 @@
+y = nil
+p y 
+puts
+y = 5
+p y
+puts
+
+p ||= 15
+p y
+print "And this is going to represent a indexed position of the character that i Want to extract\n"
+greeting = "Hello"
+extraction = 0
+letter = greeting[extraction]
+p letter
+puts
+print "Now in this is going to represent a indexed position of the character that I Want to extract\n"
+greeting = "Hello"
+extraction = 4
+letter = greeting[extraction]
+p letter
+puts
+print "In other order in this is going to represent a indexed position of the character that i Want to extract\n"
+greeting = "Hello"
+extraction = 40
+letter = greeting[extraction] # H, e, l, l, o
+p letter
+puts
+print "And rmember, the conditional assigment operator will only assign a value if the current variable\n"
+greeting = "Hello"
+extraction = 100
+letter = greeting[extraction] # H, e, l, l, o
+p letter
+letter ||= "Not found"
+p letter
+puts
