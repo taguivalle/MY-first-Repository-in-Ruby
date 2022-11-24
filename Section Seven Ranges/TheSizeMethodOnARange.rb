14ce418026d9f5468c882f0936f9e1cfce5c9ff3
taguivalle <taguivalle140965@gmail.com>
2022-11-24 08:20:20 -0500

    That's class number 88

---
+++ Section Seven Ranges/TheSizeMethodOnARange.rb
@@ -0,0 +1,5 @@
+num = 143..769 # with the two ellipses it will show the last range
+p num.size
+puts
+numbers = 143...769 # with the three ellipses it will show the last range minus one
+p numbers.size
