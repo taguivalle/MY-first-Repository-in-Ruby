9324f3790a32cda37d01f9c932b6f14189c761a9
taguivalle <taguivalle140965@gmail.com>
2022-11-24 16:52:35 -0500

    Introduction to the Arrays

---
+++ Section Seven Ranges/IntroToArrays.rb
@@ -0,0 +1,40 @@
+print "This section, Ill be introducing a new type of object, a new type of data type in Ruby and it is called array.
+But, What is an array\n? 
+First: Everything in Ruby is an object-integers, floats, Strings, Booleans, etc.\n
+Second: An array is an ordered collection of objects. It can be thought of as a list.\n
+Thirt: The array itself is an object, but it also acts a container.\n
+Fourth: An array is created with a pair of square brackets.\n"
+puts
+p []
+p [].class
+puts
+numbers = [4, 8, 15, 16, 23, 42]
+puts numbers
+p numbers
+print numbers
+puts
+toys = ["Teddy bear", "Super Soaker", "Board Game"]
+puts toys
+p toys
+print toys
+puts
+things = [4, true, "Hello", 10.99]
+puts things
+p things
+print things
+puts
+registration = [true, true, false, true, false]
+puts registration
+p registration
+print registration
+puts
+students = [["Boris", 25, true], ["Sally", 23, true], ["Ingrid", 31, false]]
+puts students
+p students
+print students
+puts
+students = [["Batmnan", 25, true], ["Kalliman", 23, true], ["Ginger", 31]]
+puts students
+p students
+print students
+puts
