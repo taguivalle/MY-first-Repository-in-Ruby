fdb81135d21a4e61a25e56520412aa9a5294516f
taguivalle <taguivalle140965@gmail.com>
2022-11-25 15:43:42 -0500

    That's class number 93

---
+++ Section Seven Ranges/ShortandSyntaxToCreateAnArrayOfString.rb
@@ -0,0 +1,6 @@
+print "This quick lesson, I'll intruduce a special syntax in Ruby to create an array of strings. let's begin by creating
+a name is variable and we have an equal sign.\n"
+puts
+name = %w[Jack jill john james Anne Maria]
+puts name
+puts
