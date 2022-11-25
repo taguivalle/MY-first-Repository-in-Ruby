86ba0ea7a56e31cf3fe1df7adf48dc72e209e4cf
taguivalle <taguivalle140965@gmail.com>
2022-11-25 11:57:09 -0500

    That's class number 92

---
+++ Section Seven Ranges/CreateAnArrayWithBracketSyntax.rb
@@ -0,0 +1,14 @@
+numbers = [4, 8, 15, 23, 42]
+toys = ["Teddy bear", "Super Soaker", "Board Game"]
+things = [4, true, "Hello world", 10.99]
+registration = [true, false, true, true, false]
+students = [["Boris", 25, true],
+        ["Slly", 23, true],
+        ["Ingrid", 21, false]]
+print students
+puts students
+print toys
+print things
+print registration
+p things
+
