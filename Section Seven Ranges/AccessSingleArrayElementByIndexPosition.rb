771e89da924e9e608fc66f18da04e8404ccb5dd5
taguivalle <taguivalle140965@gmail.com>
2022-12-12 09:42:29 -0500

    The lesson100

---
+++ Section Seven Ranges/AccessSingleArrayElementByIndexPosition.rb
@@ -0,0 +1,12 @@
+fruits = ["Apple", "Orange", "Grape", "Banana"]
+p "Hello World".length
+puts
+p fruits.length
+p fruits[0]
+puts
+fruits1 = ["Apple", "Orange", "Grape", "Banana"]
+p fruits1[0]
+p fruits1[1]
+p fruits1[2]
+p fruits1[3]
+puts
