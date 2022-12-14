58d2dc03e9c1a559bbf185bbb7b17bfd0634119b
taguivalle <taguivalle140965@gmail.com>
2022-12-14 09:46:12 -0500

    That's lesson number 104

---
+++ Section Seven Ranges/AccessSequentialSingleArrayElementWithBracketSyntax.rb
@@ -0,0 +1,17 @@
+numbers = [1, 3, 5, 7, 9, 15, 21]
+p numbers[2]
+puts
+p numbers[2, 4]
+puts
+p numbers[1, 3]
+p numbers[0, 1]
+print "element types are now displayed"
+puts
+p numbers[0].class
+p numbers[1, 3].class
+p numbers[0, 1].class
+puts
+p numbers[4, 100]
+p numbers[100]
+p numbers[4, 2]
+
