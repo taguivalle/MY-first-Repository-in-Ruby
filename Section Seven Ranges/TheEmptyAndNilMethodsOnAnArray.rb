307d62097daabeed26866bfe18c1e9bac09fe3db
taguivalle <taguivalle140965@gmail.com>
2022-12-14 11:11:51 -0500

    That's lesson number 110

---
+++ Section Seven Ranges/TheEmptyAndNilMethodsOnAnArray.rb
@@ -0,0 +1,17 @@
+puts [1, 2, 3].empty?
+puts [].empty?
+puts [].length
+puts [].length == 0
+puts [false, false, false].empty?
+puts [true, true, true].empty?
+puts [true, true, true].nil?
+puts [false, false, false].nil?
+puts 1.nil?
+puts 3.14.nil?
+puts 1.empty?
+puts 3.14.empty?
+puts
+letters = ("a".."j").to_a
+character = letters[25]
+p character
+p character.nil?
