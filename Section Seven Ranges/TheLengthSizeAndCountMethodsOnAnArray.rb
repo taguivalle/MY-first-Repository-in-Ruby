68ebff57cc98df3ca7585bbdfb8267606bead0b3
taguivalle <taguivalle140965@gmail.com>
2022-12-21 08:11:05 -0500

    That's class number 110

---
+++ Section Seven Ranges/TheLengthSizeAndCountMethodsOnAnArray.rb
@@ -0,0 +1,12 @@
+puts [1, 2 , 3].empty?
+puts [].empty?
+puts [].length
+puts [false, false, true, false].empty?
+puts [nil, nil].empty?
+puts [false, false, true, false].nil?
+puts 1.nil?
+puts 3.14.nil?
+letters = ("a".."j").to_a
+character = letters[25]
+p character
+p character.nil?
