a0e3ad67bbaa0cf319a0fdf04cffb03fc77b5182
taguivalle <taguivalle140965@gmail.com>
2022-12-21 08:51:30 -0500

    That's lesson 113

---
+++ Section Seven Ranges/ThePopMethod.rb
@@ -0,0 +1,24 @@
+arr = [1, 2, 3, 4, 5, 6, 7 , 8, 9, 10]
+p arr.length
+p arr
+arr.pop
+p arr
+arr.pop
+p arr
+puts
+array = [1, 2, 3, 4, 5, 6, 7 , 8, 9, 10, 11]
+p array
+last_item = array.pop
+p array
+p last_item
+puts
+two_items = arr.pop(2)
+p arr
+p two_items
+five_items = arr.pop(5)
+p arr
+p five_items
+puts
+item = arr.pop(1)
+p arr
+p item
