1996f2ef0e9d7121abb7785924af3aad26a9bad0
taguivalle <taguivalle140965@gmail.com>
2022-12-26 09:03:16 -0500

    That's lesson 116

---
+++ Section Seven Ranges/TheShiftAndUnshiftMethods.rb
@@ -0,0 +1,14 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # That's variable
+p arr # in this line the variable is shown
+extract = arr.shift(3) # and each number appears added plus three, three are added to each object of the variable
+p arr
+p extract
+puts
+arr.unshift(25)
+p arr
+puts
+arr.unshift(100)
+p arr
+puts
+arr.unshift(500, 300, 100)
+p arr
