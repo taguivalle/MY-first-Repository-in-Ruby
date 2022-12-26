69c48bd6f1d8d273669c7485ceb65571a7ed7a26
taguivalle <taguivalle140965@gmail.com>
2022-12-26 10:35:51 -0500

    That's lesson 119

---
+++ Section Seven Ranges/ConvertRangesToArraysWithTheToAMethod.rb
@@ -0,0 +1,11 @@
+letters_range = "A".."T"
+p letters_range
+p letters_range.to_a
+letters_range = letters_range.to_a
+p letters_range[5] # That F
+puts
+numbers_range = 415..450
+p numbers_range
+p numbers_range.to_a
+p numbers_range = numbers_range.to_a
+p numbers_range[10..15]
