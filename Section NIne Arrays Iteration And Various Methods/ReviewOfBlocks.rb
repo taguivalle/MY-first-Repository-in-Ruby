f3a939b7fc5570ca4f0e829f4114a4d3639e9917
taguivalle <taguivalle140965@gmail.com>
2022-12-27 08:37:01 -0500

    That's lesson 122

---
+++ Section NIne Arrays Iteration And Various Methods/ReviewOfBlocks.rb
@@ -0,0 +1,5 @@
+#3.times { |number| puts "I am currently on loop number #{number}"}
+5.times do |number|
+    square = number * number
+    puts "The current number is #{number} and it's square is #{square}"
+end
