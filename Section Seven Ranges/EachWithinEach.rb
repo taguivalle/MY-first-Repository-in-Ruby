13f9cdbf1daadb8df088710760a06c24057c3faa
taguivalle <taguivalle140965@gmail.com>
2022-12-27 11:13:48 -0500

    That's lesson number 125

---
+++ Section Seven Ranges/EachWithinEach.rb
@@ -0,0 +1,7 @@
+shirts = ["striped", "plain white", "plaid", "band"]
+ties = ["polka dot", "solid color", "boring"]
+shirts.each do |shirt|
+    ties.each do |tie|
+        puts "OPTION: A #{shirt} shirt and a #{tie}"
+    end
+end
