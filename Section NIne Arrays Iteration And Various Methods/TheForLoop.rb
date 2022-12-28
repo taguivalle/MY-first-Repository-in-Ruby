2da31435f643ea4a8425d59ba74f11c87a9cda5f
taguivalle <taguivalle140965@gmail.com>
2022-12-28 08:10:13 -0500

    That's lesson 126

---
+++ Section NIne Arrays Iteration And Various Methods/TheForLoop.rb
@@ -0,0 +1,26 @@
+numbers = [13, 5, 7]
+num = 10
+numbers.each {|num| puts num}
+p num
+puts
+number = [3, 5, 77]
+for nume in number do
+    puts nume
+end
+p number
+puts
+numb = [3, 5, 77]
+for numb in number do
+    puts numb
+end
+p numb
+puts
+rng = 1..10
+rng.each do |rng_number|
+    puts rng_number
+end
+rng = 1..20
+for rng_numbers in rng do
+    puts rng_numbers
+end
+p rng_numbers
