2a61ed703c9104892b290a4a06788255c3365a32
taguivalle <taguivalle140965@gmail.com>
2023-01-02 14:54:19 -0500

    That's lesson questionary eleven

---
+++ Section ten String/QuestionaryStrings.rb
@@ -0,0 +1,16 @@
+print "Review the concepts introduced so far in this section of the course.\n"
+puts
+print "question one.\n"
+p "This is a sentence".clear
+puts
+print "question two.\n"
+p "Sally sells seashells by the seashore".split("s") 
+print "question three.\n"
+p "supercalifragilisticexpialidocious".index("f")
+print "question four.\n"
+puts
+letters = ["a", "b", "c", "d", "e"].join("-")
+p letters
+puts
+print "question five.\n"
+p "leprechaun".count("e")
