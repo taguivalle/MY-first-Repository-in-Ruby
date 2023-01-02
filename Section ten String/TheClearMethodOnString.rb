38d84cdb8d0fa2b4f22ec05f417249655ae789d9
taguivalle <taguivalle140965@gmail.com>
2023-01-02 14:03:06 -0500

    That's lesson number 153

---
+++ Section ten String/TheClearMethodOnString.rb
@@ -0,0 +1,9 @@
+print "In this quick lesson, we'll take a look at the clear method on a string, and it is exactly  as the name suggests, it 
+clears the entire strings contents and makes it an empty string.\n"
+puts
+p "Blah Blah Blah Blah".clear
+puts
+word = "Goodbye"
+p word
+word.clear
+p word
