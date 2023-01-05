f21954e3983818c004d13386a31881b25f91eebb
taguivalle <taguivalle140965@gmail.com>
2023-01-05 12:50:25 -0500

    That's folder rename

--- Section four Strings/MultilineString.rb
+++
@@ -1,9 +0,0 @@
-words = <<MLS
-    this will be a multi-line string
-    when it is output
-
-the tabs will also be preserved.
-just you watch.
-Goodbye!.
-MLS
-puts words
--- Section four Strings/TheReverseMethodOnAString.rb
+++
@@ -1,8 +0,0 @@
-puts "Ruby".reverse
-puts "!@$%&".reverse
-puts "Hola Mundo@678$#%".reverse.class
-puts
-puts "Ruby is fun".upcase
-puts
-puts "Ruby is fun".upcase.reverse.downcase
-puts
