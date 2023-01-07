34355a99dc767690131d13e532b8120c453a32d6
taguivalle <taguivalle140965@gmail.com>
2023-01-07 10:51:09 -0500

    That's lesson number 177

---
+++ Section thirteen Hashes One/ConvertSymbolsToStringAndViceVersa.rb
@@ -0,0 +1,11 @@
+print "In this lesson, we'll take a look at we can convert a symbol to string and vice versa. So let's begin.\n"
+puts
+p :age.class
+p :age.to_s
+p :age.to_s.class
+p "age".to_sym
+p "age".to_sym.class
+p "School bus".to_sym
+p "School bus".to_sym.class
+puts
+
