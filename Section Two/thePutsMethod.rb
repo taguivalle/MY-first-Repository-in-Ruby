16a8a36f319d1faeb775bfe38a4294e1e2430994
code --wait <taguivalle140965@gmail.com>
2023-06-07 10:15:02 -0500

    stage one

---
+++ Section Two/thePutsMethod.rb
@@ -0,0 +1,28 @@
+print "welcom to hte next section of the course. this section is called gettin started and these lessons, we're going to explore the 
+fundamental of Ruby, the building blocks that we need to learn to understand more advanced concepts later on in the course. So int htis 
+lesson, we want to review the puts method, which we saw briefly earlier in our Hello World example. And as a reminder, we can think of a 
+method as a command in Ruby. It is an instruction thet we can ask the language to excuse for us a procedure or a behavior, something the 
+language can do for us." 
+puts
+puts 1
+print "So, for example, when we use the puts method and We provide a value like one, the puts method outputs text to the 
+screen. and another important detail is that it adds a line break to the end of the output. So outputs is a method to output some bit of 
+content and we see that right her on the right hand bellow. So puts is a method thet is avalaible to use within Ruby, but this actual act of 
+using it or executing it, writing it our file, the technical term for that, for actually executing the method is called invoking the method 
+or alternatively calling the method."
+puts
+print "So you might hear another Ruby developer say We are invokeing the puts method or We are calling puts methods. That's just the 
+technical lingo for execute or run, right? "
+puts
+print "So that's what we're doing on line one. What are oinvoking a method and We are going to be using those synonyms invoke and call 
+interchangeably throughout the course; all right"
+puts
+print "So we can of course invoke the puts method once, but we can invoke it as many times as we want. so for example, right here, We can
+write puts once again and and give it something different, like a mathematical expresion; for example, we can do one plus two and we'll see 
+thre appear on the right hand bellow"
+puts
+puts 1 + 2
+puts
+print "So once again, puts is going to add a line break to the end of whatever it outputs."
+
+
