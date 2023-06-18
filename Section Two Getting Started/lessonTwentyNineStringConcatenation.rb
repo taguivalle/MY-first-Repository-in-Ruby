bb7ccf360a92453ea864b310bd91155e2e5282ca
code --wait <taguivalle140965@gmail.com>
2023-06-18 10:48:35 -0500

    That's lesson twenty nine

---
+++ Section Two Getting Started/lessonTwentyNineStringConcatenation.rb
@@ -0,0 +1,46 @@
+print "In this lesson, we'll learn about string concatenation. To concatenate two (2) string simply means to
+join them together, to glue to one (1) string to the end of anorher. So we're joining two pieces of text,
+together and we accomplish that by using the plus (+) sign, wich is kid of fitting because of course, when 
+we're dealing with tw (2) numbers like four plus three, the plus (+) sign combines them together and the same 
+rules apply for two (2) string. Please see and run the next code in our terminal.\n\n"
+puts 4 + 3
+print "So right here, we're going to put string race, then a plus (+) sign. And on our terminal, we'll arite 
+the string car and Ruby is going to basically add them together to form the new string race car. Please see 
+and run the next code in our terminal.\n\n"
+puts 41 + 31
+puts "race" + "car"
+puts
+print "So that process of joining two (2) strings together in technical terms is called concatenation, and 
+that is a common word across different programming languages. You'll hear the therm string concatenation. All
+right. And obviosly, when you look at this example, we might think, well, why would we want to do this? Can't
+we just write a single word or a single string consisting of race car? And the answer is, you're absolutely
+right.\n\n"
+print "There's not really a need to do something like this right now. It's just for the sake of example. But 
+here's a real kind of scenario where we might need something lik this. imagine you're writing a program that 
+asks a user for their first name and then their last name separately and then you want to display their full
+name on screen.\n\n"
+print "So you may have one (1) input with the first name and a separate entry or input with their 
+second name, their last name. And we need to combine those together and maybe even add a sapce in the middle
+so it's readable. So ot's in those kinds of operations where we have dynamic data and we may not be able to 
+predict it in advance that things like become really beneficial. So these examples are a little bit contrive\n\n"
+print "But the key takeaway is the plus (+) sign what mergers or joins or concatenates two (2) strings 
+together. In Ruby. It's important to know that the way that a symbol like the plus (+) sign is going to work 
+is going to depend on what data type or object we are working with. So when were working with numbers, of 
+course Ruby code is going to simmply add them together.\n\n"
+print "But what do we think is going to happen if we do something like this? If we have a string with the 
+digit four (4) and we add on the string with the digit three, we're not going to get a string with the digit 
+seven. Because these are not numbers that were adding together. As soon as we the double quotes (""), that 
+means we're working with a string, we're working with a piece of text. Please see and run the next code in 
+our terminal.\n\n"
+puts 4 + 3
+puts "race " + "car"
+puts "4" + "3"
+puts
+print "And the way that the plus (+) sign works with a piece of text is going to be totally different. So 
+lines line thirty five (35) and line thirty seven (37) (in my terminal) are going to give we totally 
+different result. This is concatenation because we're working with strings, so we're going to get a new string 
+consisting of the characters for three in order, right? This is concatenation because this is text.\n\n"
+print "This is addition in the line thirty five (35) because these are numbers. but line thirty-seven are word 
+characters or called strings. And that jsut goes show that what objects we're working with matter because the 
+operations that we can perm on them are limited depending on what type that objects is.\n\n"
+print "So that's an introduction to string concatenation and we will see we in the nexte lesson please."
