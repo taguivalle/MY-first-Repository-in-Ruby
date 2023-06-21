69bc389e56d7b6d6ef31451a037d30767b43d780
code --wait <taguivalle140965@gmail.com>
2023-06-21 16:09:41 -0500

    That's lesson evaluation

---
+++ Section Two Getting Started/outpuAndArithmetic.rb
@@ -0,0 +1,56 @@
+print "Question one\n"
+print "¿What does the statement puts hello (into double quotes) do?\n"
+print "First: Output string without a line break\n" 
+print "Second: Output the string with a line break\n" # That's correct
+print "Third: Output the string more literally with double quotes.\n\n"
+print "Question two\n"
+print "¿What does the statement print Ruby is fun ! (into double quotes) do?\n"
+print "First: Output string without a line break\n" # That's correct
+print "Second: Output the string with a line break\n" 
+print "Third: Output the string more literally with double quotes.\n\n"
+print "Question three\n"
+print "¿What does the statement p Elephant (into double quotes) do?\n"
+print "First: Output string without a line break\n" 
+print "Second: Output the string with a line break\n" 
+print "Third: Output the string more literally with double quotes.\n\n" # That's correct
+print "Question four\n"
+print "What Ruby symbol/oparator multiplies numeric values together?\n"
+print "First: (+)\n"
+print "Second: (-)\n"
+print "third: (*)\n" # That's correct
+print "fourth: (-)\n\n"
+print "Question five\n"
+print "What Ruby symbol/oparator is used for substraction?\n"
+print "First: (+)\n"
+print "Second: (-)\n" # That's correct
+print "third: (*)\n" 
+print "fourth: (/)\n\n"
+print "Question six\n"
+print "What Ruby symbol/oparator is used for division?\n"
+print "First: (+)\n"
+print "Second: (-)\n" 
+print "third: (*)\n" 
+print "fourth: (/)\n\n" # That's correct
+print "Question seven\n"
+print "What Ruby symbol/oparator adds two values together?\n"
+print "First: (+)\n" # That's correct
+print "Second: (-)\n" 
+print "third: (*)\n" 
+print "fourth: (/)\n\n" 
+print "Question eight\n"
+print "What happens if we use the plus sign (+) with two string?\n"
+print "First: Ruby raises a typeError exception. the + operator can only be used with numbers\n"
+print "Second: Ruby adds the numeric values of the string together each letter's numeric position in the 
+alphabet\n"
+print "Ruby concatenates (i.e., combines) the two strings together\n\n" # That's correct
+print "Question nine\n"
+print "What is the name of the % operatot?\n"
+print "First: Zero slash zero\n"
+print "Second: Modulo\n" # That's correct
+print "Third: Percentage\n\n"
+print "Question ten\n"
+print "What is the evaluation of twenty three (23) % 4?\n"
+print "First: three (3)\n" # That's correct
+print "Second: zero (0)\n"
+print "Third: one (1)\n"
+print "Fourth: (2)\n\n"
