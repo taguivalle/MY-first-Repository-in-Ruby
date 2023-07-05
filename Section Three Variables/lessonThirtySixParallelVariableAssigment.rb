e7ef89ff8631983bc0fed4642aa4d08876878a5e
code --wait <taguivalle140965@gmail.com>
2023-07-05 11:23:14 -0500

    I change the name file exercise and the this lesson was thirty six parallel variables

---
+++ Section Three Variables/lessonThirtySixParallelVariableAssigment.rb
@@ -0,0 +1,53 @@
+print "In this lesson, we'll learn how we can assign multiple values to multiple variable on the same
+line. So it's little bit of a short and it is called parllel variable assigment. So let's say we have
+three variables A, B and C, and we want to assign them to the values ten, 20 and thirty respectively\n\n"
+print "So kwnowing what we currently knwo, we can write code like this here is variable A being assigned
+to the value ten (10), here is the variable B being assidned to the value twenty (20) and then the 
+variable C being assigned to value thirty (30)\n\n"
+a = 10
+b = 20
+c = 30
+print "And we can output these values to confirm that these variables have been properly assigned, we 
+can do the puts method and as a reminder, we can actually use commas to separate multiple entries that
+we would like for puts to output for us.\n\n"
+print "So we could simply write this as puts A comma, B, comma C, and we are adding a space after the 
+comma that is not technically rerequiered, but it makes this easier to read. And all this tells Ruby is
+output the values of these three variables and put a line break after each one. So we're going to see
+10, 20, and thirty in our terminal\n\n"
+puts a, b, c
+puts
+print "So here is what the shortcut looks like. What we're going to do is still use an equals because
+that is the assignment operator. That is what actually designates a value for a nmae like a variable.
+But this time around, on the terminal of the equal sign, we're going to declare all of my variables
+and separate each one with a comma.\n\n"
+print "We're going to do that all on the same line. So if we want three variables A,B and C, we're 
+simply going to write them out, just like we did here, separate by a comma. And them we're also going
+to add a space just to make it more readable.\n\n"
+print "So a then a comma and a space B, then a comma and a space C and this allows me to define three
+variables A, B and C, So on the right hand side of the equals sign, we're going to assign the same 
+number of values. And for the values, we're similarly going to separate them with a comma.\n\n"
+a, b, c =
+print "So if we have three variables on the terminal, then we nedd three values on the terminal of the 
+equal sign separated by commas. So this time around we're going to do ten (10) comma space, twenty (20)
+comma, space trhity (30).\n\n"
+a, b, c = 10, 20, 30
+puts
+print "So as many values as you have right here on the left is how many values you should have right 
+here on the right. So three variables being assigned to three values. and the way it's going to work is
+sequantially. So because A is first in line, it's going to be assigned to ten, which is the first value
+on the right hand side, B is second in line on the left, so it will be assigned to the second vlaue in 
+line on the right is twenty (20) and C the third value in line on the left or the third variable in line
+to be more precise, will be assigned to the third value on the right, which is thirty (30)\n\n"
+print "So now if we output A, B and C, once again we're going to see we get the exact same result.\n\n"
+puts a, b, c
+puts
+print "So this is just a little bit of a shortcut in two lines. We can accomplishe what we did in four
+and jsut makes the codde a little bit cleaner. Typically you'll see the syntax these variables are 
+directly connected to each other in the sense that they share something in common or are related to the
+same construct.\n\n"
+print "If they're three totally random variables that have nothing to do with each other, it's probably
+a good udea to write them out like this because it kind of better communicates that thyer're separate
+and independent.\n\n"
+print "Whenever you bundle things up to like this, it's usually to communicate that these things are 
+directly related to each other.\n\n"
+print "That's all right there is to cover in this lesson. So we'll se you in the next one."
