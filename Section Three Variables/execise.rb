d6320ddb0eb6dd231fecd1fa3049fb2d23b5aab3
code --wait <taguivalle140965@gmail.com>
2023-07-05 08:35:54 -0500

    this was the exercise of variables

---
+++ Section Three Variables/execise.rb
@@ -0,0 +1,11 @@
+# Declare a season variable and assign it a starting of "fall"
+season = "fall "
+# output the value of the season variable to the screen.
+print season
+puts season
+# Declare a hard_math and assign it the sum of 3, 4 and 5
+hard_math = 3 + 4 + 5
+# Output the values of the hard_math variable to the screen
+print hard_math
+puts
+puts hard_math
