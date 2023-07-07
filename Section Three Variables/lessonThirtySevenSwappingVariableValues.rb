462b7d3d351bf9c1933aa543e3226f55be7bc41d
code --wait <taguivalle140965@gmail.com>
2023-07-07 16:18:14 -0500

    This was lesson thirty seven

---
+++ Section Three Variables/lessonThirtySevenSwappingVariableValues.rb
@@ -0,0 +1,45 @@
+print "In this lesson, I to show you how we can swap the values of two variables. In other words, if we 
+have two variables, how can we take their respective values and flip them around, Reverse them so that
+one variables is holding the other variable's value and vice versa? so let we show what we mean.\n\n"
+print "What I'm going to do is declare two variables A and B, and I'll just assign them to arbitrary 
+values. So here's assign to one and here's B assign to and I'm jsut going to ouput the value of A and
+B, and you can see we get one and two. So what I want to do is to swap these variables and the 
+respective values.\n\n"
+a = 1
+b = 2
+puts a, b
+print "So i want a to equal to two and B to be equal to one. And we can do this all in ten line (in my
+code) in Ruby So we're going to utilize the syntax that we wrote in the previous lesson where assigned
+multiple variables to multiple values on the same line. As a reminder, we simply separate both the
+variables and the values with commas.\n\n"
+print "So we're going to stick with A and B here. so we're going to say A comma space B, so that's going
+to create or reassign two variables. And now what I want to do is provide their respective values in 
+order on the our terminal. And remember, the right hand side is goin to be evaluated first.\n\n"
+a, b = b, a # 2, 1
+print "So I want a to be equal to two, I need to provide value here that currently represents two at 
+this point in the program's execution. And that variable or that value is going to be B, them I'm going
+to put a comma and the value A. So you'll notice these sides are the inverse of each other.\n\n"
+print "And the reason this is going to work is once again because Ruby evaluates the right side of an
+expression first, the right side of an equal sign (=). So if we completely ignore this section and just
+think about what B comma A means, it means take the value of B and then follow it up with the value of 
+A, So the current value of B when this line runs two, and then the value of a is equal to one.\n\n"
+print "So that is what the right side evaluates to. it's the values two and one in order. and we're 
+going to assign the variables A and B to these two numbers in sequence. so A becomes two and B becomes
+one.\n\n"
+puts a, b
+print "so after we perform that line, we are going to swap the values of the original variables A and 
+B began as one and tw, and after this line, A and B are going to reverse or flip or swap to be two one
+So A is equal to two and B is equal to one. And this can be a helpful techinique.\n\n"
+print "Sometimes when you have variables and you want to flip their values without needing to create a
+third variable to hold an original value, we can basically kind of take their values and invert them or
+swap themp in a single line. It's one example of syntax that Ruby offers us that allows us to accomplish
+something like this in a very elegant manner. All right\n\n"
+print "So once again, we write our variable names ont he left hand side and then an equal sign. And then
+we want to write theri assignments or their values on the right hand side and that's going to be 
+aveluated first.\n\n"
+print "So if we want to reassign And B to their inverse, we simply write B and A, that's going to 
+evaluate to their original values and that's what we're going to assign A and B to on this line. It's 
+still the exact same variable principles that we're talked about in this section.\n\n"
+print "We're still comming up with a name, in this case A and B, through we are assigning to a value, 
+we're just being more creative with our use of where are these values taht we are assigning coming from.\n\n"
+print "All right, That's all there is to cover in this lesson. So I'll see you in the next one."
