5714ebe36c93af5cbd1abacc578702c17cdba644
code --wait <taguivalle140965@gmail.com>
2023-07-13 07:08:58 -0500

    This was coding Exercise Number Three Intro to Object Methods

---
+++ Section Four Object Methods/codingExerciseNumberThree.rb
@@ -0,0 +1,8 @@
+print "Use a method to determine the number of characters in.\n"
+actor = "Stallone".length
+p actor
+print "Using the above. Output the value to the screen.\n"
+actriz = "Margareth".length
+print "With all character in UPPERCASE. Output the value to the screen.\n"
+p "Stallone".upcase, "Margareth".upcase
+p "Stallone".downcase, "Margareth".downcase
