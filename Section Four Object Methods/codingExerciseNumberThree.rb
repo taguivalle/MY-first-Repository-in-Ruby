4f3ef5b3f9c75eae19af8193085d178f474a52df
code --wait <taguivalle140965@gmail.com>
2023-07-13 07:15:18 -0500

    I maked the correction and add the exercise fourty three the same

--- Section Four Object Methods/codingExerciseNumberThree.rb
+++ Section Four Object Methods/codingExerciseNumberThree.rb
@@ -6,3 +6,11 @@ actriz = "Margareth".length
 print "With all character in UPPERCASE. Output the value to the screen.\n"
 p "Stallone".upcase, "Margareth".upcase
 p "Stallone".downcase, "Margareth".downcase
+print "NOTE: coding exercise forty-three is the solution to forty-two; therefore it was not done.\n"
+actor = "Stallone"
+# Use a method to determine the number of characters in 
+# the string above. Output the value to the screen.
+puts actor.length
+# Using the string above, invoke a method to create a new string 
+# with all characters in UPPERCASE. Output the value to the screen.
+puts actor.upcase
