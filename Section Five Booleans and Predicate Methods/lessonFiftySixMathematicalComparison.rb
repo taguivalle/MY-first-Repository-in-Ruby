8451ff68cdcd375ef9b3892dfa1ea573c098e191
code --wait <taguivalle140965@gmail.com>
2023-07-24 18:37:45 -0500

    Thsi wad lesson fifty six mathematical comparison

---
+++ Section Five Booleans and Predicate Methods/lessonFiftySixMathematicalComparison.rb
@@ -0,0 +1,52 @@
+print "In this lesson, I want ti make sure that we cover all of Ruby is symbols or operators for 
+mathematical or smaller than the value on the right. So here I am asking Ruby whether one (1) in less 
+than nine (9).Is that a smaller number? An dRuby will give us boolean evaluation.\n\n"
+puts 1 < 9
+puts
+print "And of course, if I ask something like, is ten (10) less than five (5), the answer there is 
+going to be false because it is false that ten (10) is less than five (5)\n\n"
+puts 10 < 5
+puts
+print "If you want to do less than or equal to, it is going to be this symbol followed by an equal 
+sign. so here I am, cheking whether one (1) is less than or equal to five (5).\n\n"
+puts 1 <= 5
+puts
+print "And in this case, I am going to get a true and I am also going to get a true if I check if 
+five (5) is less than or equal to five (5)\n\n"
+puts 5 <= 5
+print "And then if I have a values that is greater on the left hand side, for example, is eight less 
+than or equal to five (5), the answer there, is going to be false.\n\n"
+puts 8 <= 5
+puts
+print "And I am just going to add a puts method here to buy us some space on the right hand side and
+then we will practice the inverse. So if I want to check if the vlaues on the left hand side is 
+greater than the value on the right hand side. I use this symbol right here. The greater than symbol
+is what it is actually called. So right here, I can say is ten (10) greater than five (5)?\n\n"
+puts 10 > 5
+puts
+print "The answer is going to be true. And negative numbers work just the same. So I can say is 
+negative one (1) greater tha negative five?\n\n"
+puts -1 > -5
+puts
+print "Yes, it is because it is larger. It is closer to zero (0), technically speaking. So in math 
+termns, that is a true expression. And then I can ask, is twelve (12) greater than 20 twenty? The 
+answer there is false.\n\n"
+puts 12 > 20
+puts
+print "Twelve (12) is not greater than twenty (20). I tis smaller for greter than or eqaul to, you
+guessed it, it is going to be this symbol followed by an equal sign. so I can say is eght (8) greater
+than or equal to five?\n\n"
+puts 8 >= 5
+puts
+print "The answer is yes. That is true. Is five either greater than or equal to five? The answer is
+true once again.\n\n"
+puts 5 >= 5
+puts
+print "And once again, if I say is three greater than or equal to five? No it is not. It is smaller.\n\n"
+puts 3 >= 5
+puts
+print "So that expression will produce a boolean false value. So a bunch of different ways to arrive
+at booleans in Ruby. It can be symbols like this in math, it can be the equality operator the 
+inequality operator, etcetera. Hoppefully all of this helps clarify exactly what a boolean represents.
+And with that, I will see you in the next lesson.\n\n"
+print ""
