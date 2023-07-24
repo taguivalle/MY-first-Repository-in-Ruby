6e63dd39f6e886cefae0652dea4e2c41bb705f59
code --wait <taguivalle140965@gmail.com>
2023-07-24 16:12:11 -0500

    This was lesson fourty-five the inequality operator

---
+++ Section Five Booleans and Predicate Methods/lessonFourtyFiveTheInequalityOperator.rb
@@ -0,0 +1,52 @@
+print "In this lesson I want to introduce the complementary inequality opertor. This operato checks 
+two values are not equal to one another, so it is inverive or the opossite of the equality operator.
+It will give you a true if the values are not equal to each other and a false if the values are equal
+to each other.\n\n"
+print "And the symbol for inequality operator is going to be an exclamatin point like so followed by
+an equal sign (!=). And I to thing of that exclamation point as kind of blocking or preventin the 
+equal sign. And that is how I remember kind of stopping equality, which to me indicates inequality.
+in a row. so let me show you an example right here.\n\n"
+print "I can say is ten (10) not equal to five (5). So once again, we place the symbol in between
+two objects. We add a space around both sides just for readability. And in this case, the inequality
+operator will give us true, bacause it is true that ten (10) is not equal to five (5)\n\n"
+puts 10 != 5
+puts
+print "So it is once again the opposite of equality. It will give us a true if two values are in equal 
+or unequal. So if I ask Ruby is ten (10) not equal to ten (10), the answer to that is false.\n\n"
+puts 10 != 10
+puts
+print "It is a false statement that ten (10) is not equal to ten bacause it is they are equal values
+and the same rules will apply for string comparison. So as a very example, I can ask Ruby is the 
+string hello not equal to the string of goodbye? And they are totally different strings.\n\n"
+puts "Hello" != "Goodbye"
+puts
+print "So we are going to get a true. It is true that those strings are nto equal. And similarly, if I
+ask Ruby is the string Hello?. With a capital H not equal to the string Hello. With a lowercase h. 
+Once again, we are going to get a true.\n\n"
+puts "Hello" != "hello"
+puts
+print "This satatement is true bacause these two objects were their corresponding values are nto equal.
+So that it is a true statement. And similarly, if I ask Ruby, is the string hello not equal to the 
+string? Hello. With a space at the end that is also a true statement bacause they are different 
+strings.\n\n"
+puts "Hello" != "Hello "
+puts
+print "This one has five characters and this one has six. And finally, when I actually provide two 
+equal values to the inequality operator, for example, if I ask Ruby, is the string. Hello? Not equal 
+to the string? Hello. That is going to be false because this is a false statement.\n\n"
+puts "Hello" != "Hello"
+puts
+print "It is false or untrue to say that this hello is not equal to this. Hello. Because the are equal.
+They are the same value, same string, same number of characters, same capitalization in the same order
+Right. So it is false that these two values are not equal to one another, and that is all the 
+inequality oparator is. It checks for inequality for two things not being equal to each order. Same 
+rules will apply. Of course, I to mention.\n\n"
+print "But obviously if we compare different data types the it will give us the expected value. So if
+I say is the integer five (5) not equal to a string with the character five (5) the answer is true.\n\n"
+puts 5 != "5"
+puts
+print "Those are truly not equal bacause they are not even of the same data type. Right? So operator
+this operator returns or gives back to you, the equality operator will always be the opposite or the
+inverse. So are complementary. They are fulfill the other side of the respective equation equality 
+chaecks for two things being inequality operator checks for two thing being different.\n\n"
+print "That is all there is to cover in this lesson. so I will see you in the next one."
